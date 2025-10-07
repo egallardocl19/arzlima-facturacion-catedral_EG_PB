36a521d0684262926fbffed4c0cab43ba4e3b06b
egallardocl19 <egallardo@caritaslima.org.pe>
2025-10-07 05:32:58 -0500

    Update login.php
    prueba de login

--- action/login.php
+++ action/login.php
@@ -1,35 +1,47 @@
 <?php
-	session_start();
+session_start();
 
-	if (isset($_POST['token']) && $_POST['token']!=='') {
-			
-	//Contiene las variables de configuracion para conectar a la base de datos
-	include "../config/config.php";
+if (!isset($_POST['token']) || $_POST['token'] === '') {
+    header("Location: ../");
+    exit;
+}
 
-	$email=mysqli_real_escape_string($con,(strip_tags($_POST["email"],ENT_QUOTES)));
-	$password=sha1(md5(mysqli_real_escape_string($con,(strip_tags($_POST["password"],ENT_QUOTES)))));
+// Conexión
+require_once __DIR__ . "/../config/config.php";
 
-	
-	$query1 =mysqli_query($con,"CALL session_login('$email','$password');");
-   
-    if (!$query1||mysqli_num_rows($query1)!=0){
-   
-   
-		
-		if ($row = mysqli_fetch_array($query1))
-			{
-			
-			$_SESSION['user_id']=$row['id']; // Iniciando la sesion
-			$user_id=$_SESSION['user_id'];
-			header('location: ../dashboardadmin.php'); // Redireccionando a la pagina profile.php
-			}
-			
-		}else{
-				$invalid=sha1(md5("contrasena y email invalido"));
-				header("location: ../index.php?invalid=$invalid");
-			}
-	}else{
-		header("location: ../");
-	}
+$email = mysqli_real_escape_string($con, strip_tags($_POST["email"], ENT_QUOTES));
+$pwdPlain = mysqli_real_escape_string($con, strip_tags($_POST["password"], ENT_QUOTES));
+// mismo algoritmo que usaba tu app: sha1(md5(plain))
+$pwdHash = sha1(md5($pwdPlain));
 
-?>
+$res = mysqli_query($con, "CALL session_login('$email','$pwdHash');");
+
+// ÉXITO SOLO si la consulta fue bien y hay filas
+if ($res && mysqli_num_rows($res) > 0) {
+    $row = mysqli_fetch_assoc($res);
+    $_SESSION['user_id'] = $row['id'];
+
+    // Libera resultados del SP y drena cualquier resultset adicional
+    mysqli_free_result($res);
+    while (mysqli_more_results($con) && mysqli_next_result($con)) {
+        if ($tmp = mysqli_use_result($con)) { mysqli_free_result($tmp); }
+    }
+
+    header('Location: ../dashboardadmin.php');
+    exit;
+}
+
+// FALLO: limpia resultados (si los hubiera), registra error y redirige
+if ($res instanceof mysqli_result) { mysqli_free_result($res); }
+while (mysqli_more_results($con) && mysqli_next_result($con)) {
+    if ($tmp = mysqli_use_result($con)) { mysqli_free_result($tmp); }
+}
+
+// (opcional) log interno si la consulta falló
+if ($res === false) {
+    error_log("session_login failed: (" . mysqli_errno($con) . ") " . mysqli_error($con));
+}
+
+$invalid = sha1(md5("contrasena y email invalido"));
+header("Location: ../index.php?invalid=$invalid");
+exit;
